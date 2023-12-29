c34277b5d610071dbd1cc2e47b698dd459168159
oscarbutler <>
2023-12-29 20:03:58 +0000

    Add print statements for opponent turn

--- game.py
+++ game.py
@@ -4,16 +4,19 @@ import random
 print("Welcome to a game of 21, you will have to get the closest to 21 as possible\n")
 print("Your opponent is also trying to get as close to 21 but if they reach 17 or higher they can not get another card.\n")
 
+
+
 cards = ['2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack',
-    '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack', 
-    '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack',
-    '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack']
+        '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack', 
+        '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack',
+        '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack']
 
 ace = 1 or 10
 king = 10
 queen = 10
 jack = 10
 
+print("Your Turn:")
 randomised = random.choice(cards)
 user_result_one = print(randomised)
 randomised_two = random.choice(cards)
@@ -27,10 +30,12 @@ def user():
     intro = input("Hit or Stick:\n")
     if intro.endswith('Hit'):
         user_result_one
-        print("Opponents Turn...\n")
     if intro.endswith("Stick"):
         print("Opponents Turn...\n")
 
 def opponent():
+    print("Opponents Turn...\n")
+    print(randomised_two)
 
+opponent()
 user()
