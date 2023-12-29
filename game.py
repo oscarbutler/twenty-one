96c8eab506ad8746b00cba9b5246a309d3fa8233
oscarbutler <>
2023-12-29 15:39:41 +0000

    Move the cards list outside the user function

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 
 
+cards = ['1', '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack',]
+
 def user():
     """
     This will give the user and the opponent their first card
     and will give the user the chance to get another card or
     stay with what they have.
     """
-cards = ['1', '2', '3','4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'10' , 'ace', 'king', 'queen' ,'jack',]
 intro = input("Hit or Stick:\n")
