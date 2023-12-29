9987ee390bd16b21b0ace10312d94c471af6fcc9
oscarbutler <>
2023-12-29 14:25:23 +0000

    Add explanation to user function

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 
 
 def user():
+    """
+    This will give the user and the opponent their first card
+    and will give the user the chance to get another card or
+    stay with what they have.
+    """
 cards = [1,2,3,4,5,6,7,8,9,10]
 intro = input("Hit or Stick:\n")
